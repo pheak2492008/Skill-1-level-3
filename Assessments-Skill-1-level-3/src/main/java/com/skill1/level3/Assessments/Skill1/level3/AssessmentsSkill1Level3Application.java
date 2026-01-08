3de0c3f149de7cacff00bc0d6107c17cc9510d16
sreypheak <phorn.sreypheak@institute.pse.ngo>
2026-01-08 15:01:17 +0700

    Initial commit

---
+++ Assessments-Skill-1-level-3/src/main/java/com/skill1/level3/Assessments/Skill1/level3/AssessmentsSkill1Level3Application.java
@@ -0,0 +1,13 @@
+package com.skill1.level3.Assessments.Skill1.level3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssessmentsSkill1Level3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssessmentsSkill1Level3Application.class, args);
+	}
+
+}
---
+++ Assessments-Skill-1-level-3/src/test/java/com/skill1/level3/Assessments/Skill1/level3/AssessmentsSkill1Level3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skill1.level3.Assessments.Skill1.level3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssessmentsSkill1Level3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
