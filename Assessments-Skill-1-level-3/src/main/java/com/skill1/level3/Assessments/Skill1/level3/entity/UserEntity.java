749df3619b90fa7e93e61754b27f735ee178d3a8
sreypheak <phorn.sreypheak@institute.pse.ngo>
2026-01-08 15:40:54 +0700

    add feature connects docker and mysql

---
+++ Assessments-Skill-1-level-3/src/main/java/com/skill1/level3/Assessments/Skill1/level3/entity/UserEntity.java
@@ -0,0 +1,20 @@
+package com.skill1.level3.Assessments.Skill1.level3.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+}
